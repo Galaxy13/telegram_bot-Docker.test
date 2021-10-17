448ccad0654ad8e0b05320e664bf767f156ba8c4
Galaxy13 <gen.hort54@gmail.com>
2021-10-17 21:57:37 +0300

    Creating new get_image function

--- libraries/qr_make.py
+++ libraries/qr_make.py
@@ -1,6 +1,6 @@
 import qrcode
 
-def qr_make(web_link, CHAT_ID):
+def qr_make(web_link):
     qr = qrcode.QRCode(
     version=6,
     error_correction=qrcode.constants.ERROR_CORRECT_H,
@@ -10,6 +10,10 @@ def qr_make(web_link, CHAT_ID):
 
     qr.add_data(web_link)
     qr.make(fit=True)
+    return qr
+
+def image_make(web_link, CHAT_ID):
+    qr = qr_make(web_link)
     img = qr.make_image(fill_color="black",
                     back_color='white').convert('RGB')
     img.save('test_' + CHAT_ID + '.png')
