6ed96a73d81cb19dccee2b1b95a7628b42f8ef5e
Galaxy13 <gen.hort54@gmail.comu>
2021-10-18 20:47:55 +0300

    Update: edit style of code in rules of PEP

--- libraries/qr_make.py
+++ libraries/qr_make.py
@@ -1,19 +1,21 @@
 import qrcode
 
+
 def qr_make(web_link):
     qr = qrcode.QRCode(
-    version=6,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=10,
-    border=4,
-)
+        version=6,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    )
 
     qr.add_data(web_link)
     qr.make(fit=True)
     return qr
 
+
 def image_make(web_link, CHAT_ID):
     qr = qr_make(web_link)
     img = qr.make_image(fill_color="black",
-                    back_color='white').convert('RGB')
+                        back_color='white').convert('RGB')
     img.save('test_' + CHAT_ID + '.png')
