4597e305f2c2d8f42228cdf30e7ed87b6477346b
Galaxy13 <gen.hort54@gmail.com>
2021-10-18 21:29:14 +0300

    Adding: new url validation library

---
+++ libraries/url.py
@@ -0,0 +1,8 @@
+from urllib.parse import urlparse
+
+def url_validate(link):
+    try:
+        result = urlparse(link)
+        return all([result.scheme, result.netloc])
+    except:
+        return False
