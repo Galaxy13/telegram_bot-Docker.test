ed5e879643e848fd9ae621065ba4c35d28669afd
Galaxy13 <gen.hort54@gmail.com>
2021-10-19 19:09:01 +0300

    Update: adding 2 new tests to test corner values

--- tests/test_url_validation.py
+++ tests/test_url_validation.py
@@ -1,4 +1,7 @@
 from libraries import url as val
 
+
 def test_url_validation():
-    assert val.url_validate('https://github.com/eternnoir/pyTelegramBotAPI') == True
+    assert val.url_validate('https://github.com/eternnoir/pyTelegramBotAPI')
+    assert not val.url_validate('12345')
+    assert not val.url_validate('')
